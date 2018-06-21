2f83da327fc75164b3e13ca4f898fb8a7958bf63
qqkillqq <qqkillqq@163.com>
2018-06-21 11:24:20 +0800

    demo

--- demo/index/picker.vue
+++ demo/index/picker.vue
@@ -1,17 +1,33 @@
 <template>
-    <a href="###" class="btn block radius reverse" @click="showPicker('baseData')">base picker</a>
-    <cmui-picker :visible.sync="visible"></cmui-picker>
+    <div>
+        <cmui-list>
+            <cmui-list-item v-for="item in buttonList">
+                <a href="javascript:void(0)"
+                   class="btn block radius reverse"
+                   @click="showPicker(`${item}Data`)">{{item}} picker</a>
+            </cmui-list-item>
+        </cmui-list>
+        <cmui-picker :visible.sync="visible" :data="pickerData" @select="select"></cmui-picker>
+    </div>
+
 </template>
 <script>
+    let dataList={
+      baseData:[1,2,3]
+    };
     export default{
       data:function(){
-        let baseData;
+        let buttonList=['base'];
         let visible=false;
-        return {baseData,visible}
+        return {pickerData:[],visible,buttonList}
       },
       methods:{
         showPicker(name){
-          
+          this.visible=true;
+          this.pickerData=dataList[name];
+        },
+        select(data){
+          maple.alert(JSON.stringify(data))
         }
       }
     }
