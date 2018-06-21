f65a6b744f649af38cade239d7f230464f40cf29
qqkillqq <qqkillqq@163.com>
2018-06-21 10:28:22 +0800

    picker

--- src/maple/component/pick/pick.vue
+++ src/maple/component/pick/pick.vue
@@ -53,6 +53,54 @@
 <script>
   import cmuiPopup from '../popup/popup.vue';
   import Swiper from 'swiper';
+  function formatWheelData(wheelData){
+    return _.map(wheelData,item=>{
+      if(_.isPlainObject(item)){
+        if(_.isArray(item.children)){
+          item.text=String(item.text);
+          item.children=formatWheelData(item.children);
+        }
+        return item;
+      }else{
+        return {text:item.toString(),value:item}
+      }
+    })
+  }
+  function formatData(data){
+    return _.map(data,(whleeData,wheelIndex)=>{
+      return formatWheelData(whleeData,wheelIndex);
+    })
+  }
+  function getInitData(data,selectIndex){
+    let pickerData=formatData(_.every(data,_.isArray)?data:[data]);
+    let pickerSelectIndex=(function(){
+      let colLen=pickerData.length;
+      if(_.isNumber(selectIndex)){
+        return _.fill(Array(colLen),selectIndex);
+      }else if(_.isArray(selectIndex)){
+        return _.map(selectIndex,Number);
+      }else{
+        return _.fill(Array(colLen),0)
+      }
+    })();
+    let wheelStore=[pickerData[0]];
+    for(let index=0;index<pickerData.length;index++){
+      let wheelData=pickerData[index];
+      let activeIndex=_.get(pickerSelectIndex,index,0);
+      let chiildrenData=_.get(wheelData,`[${activeIndex}].children`);
+      if(chiildrenData){
+        wheelStore[index+1]=pickerData[index+1]||[];
+        pickerData[index+1]=chiildrenData;
+      }else if(index<pickerData.length-1){
+        wheelStore[index+1]=pickerData[index+1]||[]
+      }
+    }
+    return {
+      pickerData,
+      pickerSelectIndex,
+      wheelStore
+    }
+  }
   export default {
     props:{
       data:Array
@@ -63,44 +111,31 @@
       , title:String
     },
     data:function(){
+      return getInitData(this.data,this.selectIndex);
       let _this=this;
-      function baseForm(data,index,arr){
-        return _.map(data,item=>{
-          if(_.isPlainObject(item)){
-            if(_.isArray(item.children)){
-              arr[index+1]=arr[index+1]||[];
-              item.text=String(item.text);
-              item.children=baseForm(item.children,index+1,arr);
-            }
-            return item;
-          }else{
-            return {text:item.toString(),value:item}
-          }
-        })
-      }
-      function formatData(data){
-        return _.map(data,(col,index,arr)=>{
-          return baseForm(col,index,arr);
-        })
-      }
       let pickerData=formatData(_.every(this.data,_.isArray)?this.data:[this.data]);
       let pickerSelectIndex=(function(){
         let colLen=pickerData.length;
         if(_.isNumber(_this.selectIndex)){
           return _.fill(Array(colLen),this.selectIndex);
         }else if(_.isArray(_this.selectIndex)){
-          return _.map(_this.selectIndex,Number).concat(Array(pickerData.length-_this.selectIndex.length).fill(0));
+          return _.map(_this.selectIndex,Number);
         }else{
           return _.fill(Array(colLen),0)
         }
       })();
-      let wheelStore=JSON.parse(JSON.stringify(pickerData));
-      // _.forEach(pickerData,(item,index)=>{
-      //   if(pickerSelectIndex[index]&&item.children){
-      //     wheelStore[index+1]=pickerData[index+1];
-      //     pickerData[index+1]=item;
-      //   }
-      // })
+      let wheelStore=[pickerData[0]];
+      for(let index=0;index<pickerData.length;index++){
+        let wheelData=pickerData[index];
+        let selectIndex=_.get(pickerSelectIndex,index,0);
+        let chiildrenData=_.get(wheelData,`[${selectIndex}].children`);
+        if(chiildrenData){
+          wheelStore[index+1]=pickerData[index+1]||[];
+          pickerData[index+1]=chiildrenData;
+        }else if(index<pickerData.length-1){
+          wheelStore[index+1]=pickerData[index+1]||[]
+        }
+      }
       return {
         pickerData,
         pickerSelectIndex,
@@ -116,25 +151,25 @@
         handler(value){
           this.wheels = this.wheels || [];
           let wheelWrapper = this.$refs.wheelWrapper;
-          console.log(this.pickerData)
           if(value&&!this.wheels.length){
-            this.setData(0,this.pickerSelectIndex[0]);
             this.$nextTick(()=>{
               _.forEach(this.pickerData,(item,index)=>{
                 this.initWheel(wheelWrapper,index)
               })
-              
             })
           }
           this.$emit('update:visible', value);
         }
+      },
+      data(value){
+        this.setData(value);
       }
     },
     methods:{
       initWheel(wheelWrapper,index=0){
         let _this=this;
         let initialSlide=this.pickerSelectIndex[index]||0;
-        let tag=false;
+        let isInit=true;
         _this.wheels[index]=new Swiper(wheelWrapper.children[index],{
           direction : 'vertical',
           centeredSlides : true,
@@ -150,20 +185,17 @@
           },
           on:{
             transitionEnd(){
-              if(tag&&index!=_this.pickerData.length-1){
-                console.log('tag',index,this.activeIndex)
-                _this.setData(index,this.activeIndex);
-                _this.updataWheels();
+              if(!isInit){
+                _this.updateData(index,this.activeIndex);
+                _this.updataWheels(index+1);
+              }else{
+                isInit=false;
               }
             }
           }
         });
-        if(!tag){
-          tag=!tag;
-          this.updataWheels();
-        }
       },
-      setData(index,active){
+      updateData(index,active){
         if(!_.get(this,`pickerData[${index}][${active}]`)){
           active=0;
         }
@@ -175,23 +207,22 @@
           }else{
             index++
           }
-          this.setData(index+1,this.pickerSelectIndex[index+1]);
+          this.updateData(index+1,this.pickerSelectIndex[index+1]);
         }else{
-          index++;
-          for(let len=this.pickerData.length;index<len;index++){
-            this.$set(this.pickerData[index-1],this.wheelStore[index]);
+          for(let item;item=this.pickerData[++index];){
+            this.pickerData.splice(index,1,this.wheelStore[index])
           }
         }
       },
-      updataWheels(){
+      updataWheels(fromIndex=0){
         this.$nextTick(()=>{
-          this.wheels.forEach((wheel,index)=>{
-            wheel.updateSlides();
-            wheel.slideTo(this.pickerSelectIndex[index],500,false);
-          });
+          for(let item;item=this.wheels[fromIndex++];){
+            item.updateSlides();
+            item.slideTo(this.pickerSelectIndex[fromIndex-1],false);
+          }
         })
       },
-      _getData(){
+      getData(){
         let _this=this;
         return _.map(this.wheels,(item,index)=>{
           return {
@@ -200,9 +231,23 @@
           };
         })
       },
+      setData(data,selectIndex=0){
+        _.forEach(this.wheels,item=>{
+          item.destroy()
+        })
+        this.wheels=[];
+        let wheelWrapper = this.$refs.wheelWrapper;
+        _.assign(this,getInitData(data,selectIndex));
+        this.$nextTick(()=>{
+          _.forEach(this.pickerData,(item,index)=>{
+            this.initWheel(wheelWrapper,index)
+          })
+        })
+        return;
+      },
       _ok(){
         this.visible=false;
-        let data=this._getData();
+        let data=this.getData();
         console.log(data)
         _.isFunction(this.rightFn)&&this.rightFn(data,this);
         this.$emit('select',data,this);
