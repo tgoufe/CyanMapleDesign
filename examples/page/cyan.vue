2322e60d90c3b85750975d713ab49ecb395b412a
qqkillqq <qqkillqq@163.com>
2019-10-16 21:14:42 +0800

    jd demo

--- examples/page/cyan.vue
+++ examples/page/cyan.vue
@@ -9,6 +9,8 @@
         <router-link to="cyan/button" class="padding20 dis-b">button</router-link>
         <router-link to="cyan/badge" class="padding20 dis-b">badge</router-link>
         <router-link to="cyan/form" class="padding20 dis-b">form</router-link>
+        <p>建站DEMO，在下面的案例中你将看到使用cyan的各种组合，在不使用JS和其他样式的情况下完成常见布局建设</p>
+        <router-link to="cyan/demo/productList" class="padding20 dis-b">电商网站商品列表</router-link>
     </div>
 </template>
 
---
+++ examples/page/cyan/demo/productList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="list list-col2 border light">
+            <div class="list-item" v-for="i in 20">
+                <div class="padding20">
+                    <div class="ratio-container">
+                        <div class="img-container">
+                            <img src="https://img14.360buyimg.com/mobilecms/s372x372_jfs/t1/83501/14/10883/372914/5d834928Eb7b1dfef/dd0f9595b7f5e1de.jpg!q70.dpg.webp" alt="">
+                        </div>
+                    </div>
+                    <p class="text-fixed2 marginv10 fs-13">日本进口FaSoLa 瓷砖贴纸对角贴 美缝地板贴防水耐磨地贴客厅装饰墙贴自粘 波西风情(12个/张) 大</p>
+                    <div class="flex-container">
+                        <span class="fs-10 text-red">$</span>
+                        <span class="fs-16 left text-red">222</span>
+                        <span class="badge reverse">找相似</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'productList'
+}
+</script>
+
+<style scoped>
+
+</style>
