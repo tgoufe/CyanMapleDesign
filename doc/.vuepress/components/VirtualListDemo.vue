12536af0f5108332176a4af230b0df18d6726859
chenqf <qifeng.chen1006@foxmail.com>
2019-10-23 15:20:25 +0800

    虚拟列表doc

---
+++ doc/.vuepress/components/VirtualListDemo.vue
@@ -0,0 +1,42 @@
+<template>
+    <VirtualList height="500px" :listData="data" :estimatedItemSize="100" v-slot="slotProps">
+        <div class="virtual-list-item">
+            <p><span style="color:red;">{{slotProps.item.id}}：</span>{{slotProps.item.value}}</p>
+        </div>
+    </VirtualList>
+</template>
+ 
+<script>
+
+import faker from 'faker';
+import VirtualList from '../../../src/maple/components/virtualList/main.vue';
+
+let data = [];
+for (let id = 0; id < 10000; id++) {
+    data.push({
+        id,
+        value: faker.lorem.sentences() // 长文本
+    })
+}
+
+export default {
+    name:'VirtualListDemo',
+    data(){
+        return {
+            data
+        }
+    },
+    components:{
+        VirtualList
+    }
+    
+}
+</script>
+
+<style scoped>
+.virtual-list-item{
+    border-bottom: 1px solid #999;
+    box-sizing: border-box;
+    font-size: 16px;
+}
+</style>
