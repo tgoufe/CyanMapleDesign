df31ea228a7ad164f55762339ad13d5d31d8c00c
zw251108 <zw150026@163.com>
2019-10-24 10:34:04 +0800

    添加新文章

--- src/maple/methods/cookie.js
+++ src/maple/methods/cookie.js
@@ -1,122 +1,118 @@
-import {isObject}                 from 'lodash';
-import {isTimeStr, formatTimeStr} from './util';
+import { isObject, isFunction } from 'lodash'
+import { isTimeStr, formatTimeStr } from './util'
 
-const pluses = /\+/g;
+const pluses = /\+/g
 
-function encode(s){
-	return config.raw ? s : encodeURIComponent(s);
+function encode(s) {
+  return config.raw ? s : encodeURIComponent(s)
 }
 
-function decode(s){
-	return config.raw ? s : decodeURIComponent(s);
+function decode(s) {
+  return config.raw ? s : decodeURIComponent(s)
 }
 
-function stringifyCookieValue(value){
-	return encode(config.json ? JSON.stringify(value) : String(value));
+function stringifyCookieValue(value) {
+  return encode(config.json ? JSON.stringify(value) : String(value))
 }
 
-function parseCookieValue(s){
-	if( s.indexOf('"') === 0 ){
-		// This is a quoted cookie as according to RFC2068, unescape...
-		s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
-	}
+function parseCookieValue(s) {
+  if (s.indexOf('"') === 0) {
+    // This is a quoted cookie as according to RFC2068, unescape...
+    s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\')
+  }
 
-	try{
-		// Replace server-side written pluses with spaces.
-		// If we can't decode the cookie, ignore it, it's unusable.
-		// If we can't parse the cookie, ignore it, it's unusable.
-		s = decodeURIComponent(s.replace(pluses, ' '));
-		return config.json ? JSON.parse(s) : s;
-	}
-	catch(e){
-	}
+  try {
+    // Replace server-side written pluses with spaces.
+    // If we can't decode the cookie, ignore it, it's unusable.
+    // If we can't parse the cookie, ignore it, it's unusable.
+    s = decodeURIComponent(s.replace(pluses, ' '))
+    return config.json ? JSON.parse(s) : s
+  } catch (e) {
+  }
 }
 
-function read(s, converter){
-	let value = config.raw ? s : parseCookieValue(s);
-	return $.isFunction(converter) ? converter(value) : value;
+function read(s, converter) {
+  let value = config.raw ? s : parseCookieValue(s)
+  return isFunction(converter) ? converter(value) : value
 }
 
-let config = $.cookie = function(key, value, options){
+let config = function(key, value, options) {
+  // Write
 
-	// Write
+  if (value !== undefined && !isFunction(value)) {
+    options = Object.assign({}, config.defaults, options)
 
-	if( value !== undefined && !$.isFunction(value) ){
-		options = $.extend({}, config.defaults, options);
+    if (typeof options.expires === 'number') {
+      let days = options.expires
+      let t = options.expires = new Date()
+      t.setTime(+t + days * 864e+5)
+    }
+    return (
+      document.cookie = [
+        encode(key),
+        '=',
+        stringifyCookieValue(value),
+        options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+        options.path ? '; path=' + options.path : '',
+        options.domain ? '; domain=' + options.domain : '',
+        options.secure ? '; secure' : ''
+      ].join(''))
+  }
 
-		if( typeof options.expires === 'number' ){
-			let days = options.expires,
-				t = options.expires = new Date();
-			t.setTime(+t + days * 864e+5);
-		}
-		return (
-			document.cookie = [
-				encode(key),
-				'=',
-				stringifyCookieValue(value),
-				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-				options.path ? '; path=' + options.path : '',
-				options.domain ? '; domain=' + options.domain : '',
-				options.secure ? '; secure' : ''
-			].join(''));
-	}
+  // Read
 
-	// Read
+  let result = key ? undefined : {}
 
-	let result = key ? undefined : {};
+  // To prevent the for loop in the first place assign an empty array
+  // in case there are no cookies at all. Also prevents odd result when
+  // calling $.cookie().
+  let cookies = document.cookie ? document.cookie.split('; ') : []
 
-	// To prevent the for loop in the first place assign an empty array
-	// in case there are no cookies at all. Also prevents odd result when
-	// calling $.cookie().
-	let cookies = document.cookie ? document.cookie.split('; ') : [];
+  for (let i = 0, l = cookies.length; i < l; i++) {
+    let parts = cookies[i].split('=')
+    let name = decode(parts.shift())
+    let cookie = parts.join('=')
 
-	for( let i = 0, l = cookies.length; i < l; i++ ){
+    if (key && key === name) {
+      // If second argument (value) is a function it's a converter...
+      result = read(cookie, value)
+      // break;
+    }
 
-		let parts = cookies[i].split('=');
-		let name = decode(parts.shift());
-		let cookie = parts.join('=');
+    // Prevent storing a cookie that we couldn't decode.
+    if (!key && (
+      cookie = read(cookie)) !== undefined) {
+      result[name] = cookie
+    }
+  }
+  return result
+}
 
-		if( key && key === name ){
-			// If second argument (value) is a function it's a converter...
-			result = read(cookie, value);
-			//break;
-		}
+config.defaults = {}
 
-		// Prevent storing a cookie that we couldn't decode.
-		if( !key && (
-			cookie = read(cookie)) !== undefined ){
-			result[name] = cookie;
-		}
-	}
-	return result;
-};
+let removeCookie = function(key, options) {
+  if (cookie(key) === undefined) {
+    return false
+  }
 
-config.defaults = {};
+  // Must not alter options, thus extending a fresh object...
+  cookie(key, '', Object.assign({}, options, { expires: -1 }))
+  return !cookie(key)
+}
+/* ---------- copy jquery.cookie end---------------- */
 
-$.removeCookie = function(key, options){
-	if( $.cookie(key) === undefined ){
-		return false;
-	}
+const cookie = function() {
+  if (arguments[2] && isObject(arguments[2])) {
+    if (isTimeStr(arguments[2]['expires'])) {
+      arguments[2]['expires'] = new Date(new Date().getTime() + formatTimeStr(arguments[2]['expires']))
+    }
+  }
+  return cookie.apply(this, arguments)
+}
+cookie.remove = function() {
+  return removeCookie.apply(this, arguments)
+}
 
-	// Must not alter options, thus extending a fresh object...
-	$.cookie(key, '', $.extend({}, options, {expires: -1}));
-	return !$.cookie(key);
-};
-/*---------- copy jquery.cookie end----------------*/
+export { cookie }
 
-
-const cookie = function(){
-	if( arguments[2] && isObject(arguments[2]) ){
-		if( isTimeStr(arguments[2]['expires']) ){
-			arguments[2]['expires'] = new Date(new Date().getTime() + formatTimeStr(arguments[2]['expires']));
-		}
-	}
-	return $.cookie.apply(this, arguments);
-};
-cookie.remove = function(){
-	return $.removeCookie.apply(this, arguments);
-};
-
-export {cookie};
-
-export default cookie;
+export default cookie
--- src/maple/methods/session.js
+++ src/maple/methods/session.js
@@ -1,47 +1,43 @@
-import {isObject}                         from 'lodash';
-import {sessionStorage as supportSession} from './support';
+import { isObject } from 'lodash'
+import { sessionStorage as supportSession } from './support'
 
-let sessionKey = 'tg_session_cache',
-	cache = supportSession ? sessionStorage : false,
-	data = supportSession ? JSON.parse((
-		cache.getItem(sessionKey) || JSON.stringify({}))) : {},
-	sessionData = function(){
-		let length = arguments.length;
-		if( !length ){
-			return data;
-		}
-		else if( length === 1 ){
-			if( isObject(arguments[0]) ){
-				data = $.extend({}, data, arguments[0]);
-				cache && cache.setItem(sessionKey, JSON.stringify(data));
-			}
-			else{
-				return data[arguments[0]];
-			}
-		}
-		else if( length === 2 ){
-			data[arguments[0]] = arguments[1];
-			cache && cache.setItem(sessionKey, JSON.stringify(data));
-		}
-	};
-sessionData.remove = function(key){
-	let length = arguments.length;
-	if( !length ){
-		this.removeAll();
-	}
-	else{
-		for( let i = 0; i < length; i++ ){
-			data[arguments[i]] = undefined;
-			delete data[arguments[i]];
-		}
-		cache && cache.setItem(sessionKey, JSON.stringify(data));
-	}
-};
-sessionData.removeAll = function(){
-	data = {};
-	cache && cache.setItem(sessionKey, JSON.stringify(data));
-};
+let sessionKey = 'tg_session_cache'
+let cache = supportSession ? sessionStorage : false
+let data = supportSession ? JSON.parse((
+  cache.getItem(sessionKey) || JSON.stringify({}))) : {}
+let sessionData = function() {
+  let length = arguments.length
+  if (!length) {
+    return data
+  } else if (length === 1) {
+    if (isObject(arguments[0])) {
+      data = Object.assign({}, data, arguments[0])
+      cache && cache.setItem(sessionKey, JSON.stringify(data))
+    } else {
+      return data[arguments[0]]
+    }
+  } else if (length === 2) {
+    data[arguments[0]] = arguments[1]
+    cache && cache.setItem(sessionKey, JSON.stringify(data))
+  }
+}
+sessionData.remove = function(key) {
+  let length = arguments.length
+  if (!length) {
+    this.removeAll()
+  } else {
+    for (let i = 0; i < length; i++) {
+      data[arguments[i]] = undefined
+      delete data[arguments[i]]
+    }
+    cache && cache.setItem(sessionKey, JSON.stringify(data))
+  }
+}
+sessionData.removeAll = function() {
+  data = {}
+  cache && cache.setItem(sessionKey, JSON.stringify(data))
+}
 
-export {sessionData};
+export { sessionData }
 
-export default sessionData;
+export default sessionData
