65a7773b782f86bf5b60cbf999091d9e73210dff
zw251108 <zw150026@163.com>
2019-10-24 10:34:04 +0800

    修正错误的函数引用

--- src/maple/methods/index.js
+++ src/maple/methods/index.js
@@ -1,17 +1,17 @@
-import device              from './device.js'
+import device from './device.js'
 import { isInView, ready } from './dom.js'
-import load        from './load.js'
-import log         from './log.js'
-import scrollBar   from './scroll_bar.js'
-import shake       from './shake.js'
-import style       from './style.js'
-import time        from './time.js'
-import url         from './url.js'
-import copy        from './copy.js'
-import fullScreen  from './fullScreen.js'
-import cookie      from './cookie.js'
-import localData   from './local.js';
-import sessionData from './session';
+import load from './load.js'
+import log from './log.js'
+import scrollBar from './scroll_bar.js'
+import shake from './shake.js'
+import style from './style.js'
+import time from './time.js'
+import url from './url.js'
+import copy from './copy.js'
+import fullScreen from './fullScreen.js'
+import cookie from './cookie.js'
+import localData from './local.js'
+import sessionData from './session'
 
 export default {
   device,
