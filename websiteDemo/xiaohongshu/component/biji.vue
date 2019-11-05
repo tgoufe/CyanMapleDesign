b84c97bcc6a41a35b8d9cc7a5b3146d4f01e2d2c
qqkillqq <qqkillqq@163.com>
2019-11-05 20:51:02 +0800

    readme

---
+++ websiteDemo/xiaohongshu/component/biji.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="row">
+            <div v-for="(span, iout) in 2" :key="iout" class="span6">
+              <div v-for="(item,index) in span==1?imgL:imgR" :key="index" class="shadow marginb20 radius">
+                <img :src="item" alt="">
+                <div class="padding20">
+                  <p class=" fs-13 marginb20">
+      {{ intro[(iout)*10+index] }}
+      </p>
+                  <div class="flex-container">
+                    <div class="img-container round full" style="width:25px">
+                      <img :src="avatar[(iout)*10+index]" alt="">
+                    </div>
+                    <span class="marginl10 fs-12 text-light left text-limit1">{{ name[(iout)*10+index] }}</span>
+                    <i class="iconfont icon-favorites text-light"></i>
+                    <span class="text-light fs-11 marginl10">4224</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+</template>
+<script>
+import { index, intro, avatar, name, cube, imageGrid } from '../data/img.json'
+export default {
+  name: 'biji',
+  data() {
+    return {
+      imgL: index.slice(index.length / 2),
+      imgR: index.slice(0, index.length / 2),
+      index,
+      intro,
+      avatar,
+      name,
+      cube,
+      imageGrid
+    }
+  }
+}
+</script>
---
+++ websiteDemo/xiaohongshu/page/brands.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="">
+    <div class="ratio-container ratio-container-2x1">
+      <div class="img-container flex-y">
+        <img src="https://ci.xiaohongshu.com/0285c6ad-e8c8-400d-865f-02b233d87d55?imageView2/2/w/1200/q/90" alt="">
+      </div>
+      <div class="mask" style="z-index:auto;position:absolute"></div>
+      <div style="top:auto" class="flex-container padding30">
+        <div class="ratio-container img-container radius marginr20" style="width:70px">
+          <img src="https://ci.xiaohongshu.com/479e47ac-6135-47c1-9394-a7feade1b3f8?imageView2/3/w/420/h/600/format/jpg/q/90" alt="">
+        </div>
+        <div class="flex1">
+          <p class="text-white fs-18">
+          珂莱欧
+          </p>
+          <div class="fs-12 text-lighter">
+            <p>CLIO</p>
+            <p>9315篇笔记</p>
+          </div>
+        </div>
+        <div class="badge big red bottom pill">
+        关注
+        </div>
+      </div>
+    </div>
+
+    <div class="paddingh30 margint30 borderb flex-container paddingb20">
+      <i class="iconfont icon-account marginr10"></i>
+      <span class="left">CLIO珂莱欧</span>
+      <span class="text-light">2601个粉丝</span>
+      <div class="iconfont icon-more text-light"></div>
+    </div>
+
+    <div class="padding30 pos-r text-dark borderb">
+      <p :class="{'text-limit2':showmore}" @click="showmore=!showmore">
+        来自韩国的品牌。珂莱欧将多年专业的彩妆经验，创新性的融入高品质的产品中，让各肤质类型和各年龄段的人群都能轻松搞定如专业彩妆师级别精致妆容，打造更时髦的都市女性；菲丽菲拉针对妙龄少女，提供更具时尚潮流、个性化需求的魅力彩妆；果达儿专注寻找对肌肤有益的自然原料，研究改善肌肤的配方让皮肤更活力健康。
+      </p>
+    </div>
+
+    <div class="">
+      <cmui-tabbar class="">
+        <cmui-tabbar-item title="首页">
+          <biji></biji>
+        </cmui-tabbar-item>
+        <cmui-tabbar-item title="图集">
+          <div class="list list-col3">
+            <div v-for="item in imageGrid" class="list-item padding2">
+              <div class="ratio-container">
+                <img :src="item" alt="">
+              </div>
+            </div>
+          </div>
+        </cmui-tabbar-item>
+        <cmui-tabbar-item title="笔记">
+          <biji></biji>
+        </cmui-tabbar-item>
+        <cmui-tabbar-item title="商品">
+          <div class="list list-col2 padding20">
+            <div v-for="item in imageGrid" class="list-item padding6">
+              <div class="bg-white shadow radius">
+                <div class="ratio-container img-container">
+                  <img :src="item" alt="">
+                </div>
+                <div class="padding20">
+<p class="text-darker marginb10 fs-13">
+  韩国·不泛油光女神肌
+  </p>
+                  <p class="text-light marginb10 fs-13">
+  CLIO珂莱欧 遮瑕气垫粉底液+替换装 15g #2自然偏白
+  </p>
+                  <div class="badge red small">
+立减5
+</div>
+                  <span class="text-red dis-b">￥38</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </cmui-tabbar-item>
+      </cmui-tabbar>
+    </div>
+</div>
+</template>
+<script>
+import biji from '../component/biji'
+import { imageGrid } from '../data/img.json'
+export default {
+  name: 'brands',
+  components: { biji },
+  data() {
+    return {
+      showmore: true,
+      imageGrid
+    }
+  }
+}
+</script>
