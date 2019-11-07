b4c0083f10f7a194db5a408df19165e9abed8528
zhangyue <dlpuzhangyue@163.com>
2019-11-07 17:03:50 +0800

    网易严选demo-识物完成

---
+++ websiteDemo/wangyiyanxuan/page/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="header">
+        $END$
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+    data () {
+      return {}
+    },
+    created () {},
+    methods: {},
+  }
+</script>
+<style scoped></style>
