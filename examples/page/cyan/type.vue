e5fc68b52ef76ec1e4f846b532e6a23855335769
王磊 <11169514@qq.com>
2019-11-08 14:36:54 +0800

    排版 样式

--- examples/page/cyan/type.vue
+++ examples/page/cyan/type.vue
@@ -1,40 +1,59 @@
 <template>
     <div>
-        <p>彩色文本颜色</p>
-        <p v-for="color in colors" :class="[`text-${color}`]" class="fs-20">text-{{color}}</p>
-        <p>黑白文本颜色</p>
-        <p v-for="color in grays" :class="[`text-${color}`]" class="fs-20">text-{{color}}</p>
-        下面展示的文本为两种尺寸，在375的设备上他们显示效果是相同的，但是在其他尺寸上REM会缩放而PX不会
-        <p>固定PX</p>
-        <span v-for="size in 11" :class="[`fs-${size+9}`]">{{size+9}}px<br v-if="size===5"></span>
-        <p>相对REM</p>
-        <span v-for="size in 11" :class="[`frs-${size*2+18}`]">{{size*2+18}}rem<br v-if="size===5"></span>
-        <p>文本排列方式</p>
-        <div v-for="name in styles" class="flex-container">
-            <span class="marginr20" style="width:120px">{{name}}</span>
-            <p class="flex1" :class="[name]">some text</p>
+        <p class="text-white fs-18 paddingh50 paddingv30 bg-black">文字颜色与尺寸</p>
+        <p class="title paddingv20 paddingh30">彩色文本颜色</p>
+        <div class="row">
+            <div class="span4 fs-16 text-center" v-for="color in colors" :class="[`text-${color}`]">
+                <div class="paddingv30">text-{{color}}</div>
+            </div>
         </div>
-        <div class="text-container">
-            <span class="fs-20 text-vcenter">vcente</span>
-            <span class="fs-10 text-vcenter">vcente</span>
+        <p class="title paddingv20 paddingh30">黑白文本颜色</p>
+        <div class="row">
+            <div class="span4 fs-16 text-center" v-for="color in grays" :class="[`text-${color}`]">
+                <div class="paddingv30">text-{{color}}</div>
+            </div>
         </div>
-        <br>
-        <div class="text-container">
-            <span class="fs-20 text-vtop">vtop</span>
-            <span class="fs-10 text-vtop">vtop</span>
+        <p class="text-white fs-18 paddingh50 paddingv30 bg-black">下面展示的文本为两种尺寸，在375的设备上他们显示效果是相同的，但是在其他尺寸上REM会缩放而PX不会</p>
+        <p class="title paddingv20 paddingh30">固定PX</p>
+        <div class="row">
+            <div class="col3 text-center" v-for="size in 11" :class="[`fs-${size+9}`]">
+                <div class="flex-container center" style="line-height: 56px;">{{size+9}}px</div>
+            </div>
         </div>
-        <br>
-        <div class="text-container">
-            <span class="fs-20 text-vbottom">vbottom</span>
-            <span class="fs-10 text-vbottom">vbottom</span>
+        <p class="title paddingv20 paddingh30">相对REM</p>
+        <div class="row">
+            <div class="col3 text-center"  v-for="size in 11" :class="[`frs-${size*2+18}`]">
+                <div class="flex-container center" style="line-height: 56px;">{{size*2+18}}rem</div>
+            </div>
         </div>
-        <p>文本限制下面的文本被限制到2行</p>
-        <p class="text-limit2">
-            朝阳初升，云蒸霞蔚，苍莽群山在重重朝雾中绵延起伏。
-            姬凤瑶在冲霄峰崖顶临崖而坐，脚下是千尺绝壁、层云环绕。眼前的景色怎一个美字了得，但她的心情却有些惆怅。
-            一天前，她又魂穿了。
-            嗯，“又”。
-        </p>
+        <p class="text-white fs-18 paddingh50 paddingv30 bg-black">文本排列方式</p>
+        <div v-for="name in styles">
+            <p class="title paddingv20 paddingh30">{{name.replace("-", " ")}}</p>
+            <div class="fs-16 paddingv30 paddingh50" :class="[name]">some text</div>
+        </div>
+        <div>
+            <p class="title paddingv20 paddingh30">vcente</p>
+            <div class="fs-16 paddingh50 text-vcenter"" style="height: 60px;">vcente</div>
+        </div>
+        <div>
+            <p class="title paddingv20 paddingh30">vtop</p>
+            <div class="fs-16 paddingh50 text-vtop" style="height: 60px;">vtop</div>
+        </div>
+        <div>
+            <p class="title paddingv20 paddingh30">vbottom</p>
+            <div class="fs-16 paddingh50 text-vbottom" style="height: 60px;">vbottom</div>
+        </div>
+        <p class="text-white fs-18 paddingh50 paddingv30 bg-black">文本限制</p>
+        <p class="title paddingv20 paddingh30">文本限制到两行</p>
+        <div class="paddingv20 paddingh50">
+            <p class="text-limit2">
+                朝阳初升，云蒸霞蔚，苍莽群山在重重朝雾中绵延起伏。
+                姬凤瑶在冲霄峰崖顶临崖而坐，脚下是千尺绝壁、层云环绕。眼前的景色怎一个美字了得，但她的心情却有些惆怅。
+                一天前，她又魂穿了。
+                嗯，“又”。
+            </p>
+        </div>
+        
     </div>
 </template>
 
@@ -80,5 +99,10 @@ export default {
 </script>
 
 <style scoped>
-
+p.title{
+    font-size: 14px;
+    background: rgba(0,0,0,0.04);
+    color: #666666;
+    line-height: 20px;
+}
 </style>
