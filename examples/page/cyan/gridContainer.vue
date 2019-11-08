0b69e31550a05fa89e0c207fc219ba551b3520fb
王磊 <11169514@qq.com>
2019-11-08 16:13:53 +0800

    栅格布局 样式2

--- examples/page/cyan/gridContainer.vue
+++ examples/page/cyan/gridContainer.vue
@@ -1,39 +1,39 @@
 <template>
 <div>
-    <p class="text-black fs-18 padding30">layout</p>
-    <p class="title paddingv20 paddingh30">grid container</p>
+    <p class="text-black fs-18 padding30">Layout</p>
+    <p class="title paddingv20 paddingh30">Grid container</p>
     <div class="container fs-14 padding30" style="color: #00F8F8;">
         <div class="row">
             <div class="span4" v-for="item in 3">
-                <div class="gridDemo" style="background: #00BABA;">span4</div>
+                <div class="gridDemo" style="background: #00BABA;">Span4</div>
             </div>
         </div>
         <div class="row margint20">
             <div class="span3" v-for="item in 4">
-                <div class="gridDemo" style="background: #007C7C;">span3</div>
+                <div class="gridDemo" style="background: #007C7C;">Span3</div>
             </div>
         </div>
         <div class="row margint20">
             <div class="col3" v-for="item in 5">
-                <div class="gridDemo" style="background: #003E3E;">col3</div>
+                <div class="gridDemo" style="background: #003E3E;">Col3</div>
             </div>
         </div>
     </div>
-    <p class="title paddingv20 paddingh30">box container</p>
+    <p class="title paddingv20 paddingh30">Box container</p>
     <div class="container fs-14 padding30" style="color: #00F8F8;">
         <div class="box-row">
             <div class="box-span4" v-for="item in 3">
-                <div class="gridDemo" style="background: #00BABA;">span4</div>
+                <div class="gridDemo" style="background: #00BABA;">Span4</div>
             </div>
         </div>
         <div class="box-row margint20">
             <div class="box-span3" v-for="item in 4">
-                <div class="gridDemo" style="background: #007C7C;">span3</div>
+                <div class="gridDemo" style="background: #007C7C;">Span3</div>
             </div>
         </div>
         <div class="box-row margint20">
             <div class="box-col3" v-for="item in 5">
-                <div class="gridDemo" style="background: #003E3E;">col3</div>
+                <div class="gridDemo" style="background: #003E3E;">Col3</div>
             </div>
         </div>
     </div>
