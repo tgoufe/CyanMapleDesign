7f68e95dba3ebe92ba5bdf5c8458eb3a3aeaa3d3
王磊 <11169514@qq.com>
2019-11-13 15:40:21 +0800

    badge 样式

--- examples/page/cyan/badge.vue
+++ examples/page/cyan/badge.vue
@@ -1,35 +1,39 @@
 <template>
     <div>
-        <p>默认</p>
-        <div class="badge">默认</div>
-        <p>色彩</p>
+        <p class="title paddingv20 paddingh30">默认</p>
+        <div class="padding20">
+            <div class="badge">默认</div>
+        </div>
+        <p class="title paddingv20 paddingh30">色彩</p>
         <div v-for="color in colors" class="badge margin20" :class="[color]">badge</div>
-        <p>圆角</p>
+        <p class="title paddingv20 paddingh30">圆角</p>
         <div v-for="color in colors" class="badge margin20 radius" :class="[color]">badge radius</div>
-        <p>翻转</p>
+        <p class="title paddingv20 paddingh30">翻转</p>
         <div v-for="color in colors" class="badge margin20 reverse" :class="[color]">badge reverse</div>
-        <p>圆角+翻转</p>
+        <p class="title paddingv20 paddingh30">圆角+翻转</p>
         <div v-for="color in colors" class="badge margin20 reverse radius" :class="[color]">radius reverse</div>
-        <p>圆</p>
+        <p class="title paddingv20 paddingh30">圆</p>
         <div v-for="color in colors" class="badge margin20  round" :class="[color]">B</div>
-        <p>圆+翻转</p>
+        <p class="title paddingv20 paddingh30">圆+翻转</p>
         <div v-for="color in colors" class="badge margin20 reverse  round" :class="[color]">B</div>
-        <p>方块</p>
+        <p class="title paddingv20 paddingh30">方块</p>
         <div v-for="color in colors" class="badge margin20  square" :class="[color]">B</div>
-        <p>方块+翻转</p>
+        <p class="title paddingv20 paddingh30">方块+翻转</p>
         <div v-for="color in colors" class="badge margin20 reverse square" :class="[color]">B</div>
-        <p>药片</p>
+        <p class="title paddingv20 paddingh30">药片</p>
         <div v-for="color in colors" class="badge margin20  pill" :class="[color]">B</div>
-        <p>药片+翻转</p>
+        <p class="title paddingv20 paddingh30">药片+翻转</p>
         <div v-for="color in colors" class="badge margin20 reverse pill" :class="[color]">B</div>
-        <p>旗帜</p>
-        <div v-for="color in colors" class="badge margin20 flag" :class="[color]">B</div>
-        <p>尺寸</p>
+        <p class="title paddingv20 paddingh30">旗帜</p>
+        <div class="paddingb20">
+            <div v-for="color in colors" class="badge margin20 flag" :class="[color]">B</div>
+        </div>
+        <p class="title paddingv20 paddingh30">尺寸</p>
         <div class="margin20 badge small">small</div>
         <div class="margin20 badge">default</div>
         <div class="margin20 badge big">big</div>
-        <p>组</p>
-        <div class="badge-group">
+        <p class="title paddingv20 paddingh30">组</p>
+        <div class="badge-group padding20">
             <div class="badge reverse blue radius">上一页</div>
             <div class="badge reverse blue radius" v-for="i in 3">{{i}}</div>
             <div class="badge reverse blue radius">下一页</div>
