843248c37acac1d098d80b1fc8089bf61510d774
wanghanshen <50658110+wang-han-sheng@users.noreply.github.com>
2019-11-19 11:00:44 +0800

    add collpase、countdown demo

---
+++ doc/.vuepress/components/Com/Countdown-3.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <cmui-countdown :shouDay="true" :showMilli="true" :end-time="+new Date()+10000000"></cmui-countdown>
+    </div>
+</template>
+<script>
+export default {
+    name:'Countdown-1'
+}
+</script>
