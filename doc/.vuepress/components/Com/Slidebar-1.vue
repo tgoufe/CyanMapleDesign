020766c21c86b51c7c22e98de9d4f563bcf55174
guoyuan <243341386@qq.com>
2019-11-21 16:00:56 +0800

    Update Slidebar-1.vue
    slidebar_com edit

--- doc/.vuepress/components/Com/Slidebar-1.vue
+++ doc/.vuepress/components/Com/Slidebar-1.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        <cmui-slidebar v-model="value1" :step="step"></cmui-slidebar>
+        <p>设定滑动范围</p>
+        <cmui-slidebar v-model="value1" :range="range1"></cmui-slidebar>
+        <p>{{value1}}</p>
+        <hr>
+        <p>设定每次滑动最小单位</p>
+        <cmui-slidebar v-model="value2" :step="step2"></cmui-slidebar>
+        <p>{{value2}}</p>
+        <hr>
+        <p>设定change事件</p>
+        <cmui-slidebar v-model="value3" :step="step3" class="marginb20" @change="changeHandle"></cmui-slidebar>
+        <p>{{value3}}</p>
     </div>
 </template>
 
@@ -8,12 +18,18 @@
 	export default {
 		name: "Slidebar-1",
 		methods:{
-
+			changeHandle(value){
+				alert(value)
+			}
         },
         data() {
 			return {
 				value1: 90,
-                step: 10
+				value2: 10,
+				value3: 20,
+				range1: 255,
+                step2: 10,
+                step3: 1
             }
         }
 	};
