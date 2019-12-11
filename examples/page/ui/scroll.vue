26e488ab0908c4e1011aafb500c27bd36f613631
svgShow <11169514@qq.com>
2019-12-11 16:39:57 +0800

    scroll 样式更新

--- examples/page/ui/scroll.vue
+++ examples/page/ui/scroll.vue
@@ -1,8 +1,54 @@
 <template>
-	<div class="padding20">
+	<div style="background: #f5f5f5;">
 		<div class="tag-container example">
-			<cmui-scroll ref="xxx" direction="v" style="height:400px" :pull-text="['下拉刷新','释放刷新','上拉加载','释放加载']" :free-mode="true" :watch="imageList" @pullStart="start" @pullEnd="end" @rendered="rendered">
-				<cmui-scroll-item v-for="(k,i) in imageList" :key="i">
+			<cmui-scroll ref="xxx" direction="v" style="height:600px" :pull-text="['下拉刷新','释放刷新','上拉加载','释放加载']" :free-mode="true" :watch="imageList" @pullStart="start" @pullEnd="end" @rendered="rendered">
+				<cmui-scroll-item key="1">
+					<div class="paddingh40 paddingb30 bg-white">
+						<div class="paddingv30 fs-16">
+							下拉当前列表可触发刷新
+						</div>
+						<div class="flex-container center" style="background: #f5f5f5;height:188px;">
+							<img :src="imageList" height="32" alt="">
+						</div>
+					</div>
+				</cmui-scroll-item>
+				<cmui-scroll-item key="2">
+					<div class="margint10 bg-white padding40 flex-container vfull">
+						<div class="flex-container-col left">
+							<div class="fs-18">下拉当前列表可触发刷新<br>上滑当前列表可触发加载</div>
+							<div class="fs-12 text-light"><span class="marginr20">SHAYI</span><span class="marginr20">1075评论</span>5小时前</div>
+						</div>
+						<div class="flex-container center" style="background: #f5f5f5;height:100px;width:100px;">
+							<img :src="imageList" style="transform: rotate(180deg);" height="32" alt="">
+						</div>
+					</div>
+				</cmui-scroll-item>
+				<cmui-scroll-item key="3">
+					<div class="margint10 bg-white padding40 flex-container vfull">
+						<div class="flex-container center" style="background: #f5f5f5;height:100px;width:100px;">
+							<img :src="imageList" height="32" alt="">
+						</div>
+						<div class="flex-container-col left marginl40">
+							<div class="fs-18">下拉当前列表可触发刷新<br>上滑当前列表可触发加载</div>
+							<div class="fs-12 text-light"><span class="marginr20">SHAYI</span><span class="marginr20">1075评论</span>5小时前</div>
+						</div>
+					</div>
+				</cmui-scroll-item>
+				<cmui-scroll-item key="4">
+					<div class="paddingh40 bg-white margint10">
+						<div class="paddingv30 fs-16">
+							上滑当前列表可触发加载
+						</div>
+						<div class="flex-container marginh-n5">
+							<div class="flex-container center flex1 marginh5" v-for="item in 3" style="background: #f5f5f5;height:60px;">
+								<img :src="imageList" height="32" alt="" style="transform: rotate(180deg);">
+							</div>
+						</div>
+						
+						<div class="fs-12 text-light paddingv20"><span class="marginr20">SHAYI</span><span class="marginr20">1075评论</span>5小时前</div>
+					</div>
+				</cmui-scroll-item>
+				<!-- <cmui-scroll-item v-for="(k,i) in imageList" :key="i">
 					<div class="flex-container padding10">
 						<div class="ratio-container" style="width:100px">
 							<div class="img-container border marginr10 round flex-x">
@@ -13,17 +59,20 @@
 							this is image {{ i }}
 						</div>
 					</div>
-				</cmui-scroll-item>
+				</cmui-scroll-item> -->
 			</cmui-scroll>
 		</div>
 	</div>
 </template>
+<style>
+	
+</style>
 <script type="text/javascript">
-import img from '@assets/img.js'
+import img from '@assets/scroll.png'
 export default {
   data: function() {
     return {
-      imageList: [...img.whiteList, ...img.blackList]
+      imageList: img
     }
   },
   mounted() {
@@ -34,7 +83,7 @@ export default {
       maple.alert('下拉刷新事件被触发')
     },
     end() {
-      this.imageList = this.imageList.concat([...img.whiteList, ...img.blackList])
+      // this.imageList = this.imageList.concat([...img.whiteList, ...img.blackList])
     },
     rendered(vm) {
       console.log(vm.swiper)
