8b23cb24e44b672c57720e9969263e22ae326c35
wanghanshen <50658110+wang-han-sheng@users.noreply.github.com>
2019-12-16 15:59:22 +0800

    eleme new class

--- websiteDemo/elem/page/home.vue
+++ websiteDemo/elem/page/home.vue
@@ -9,7 +9,7 @@
         <a href class="bg-white paddingv40 paddingh300 text-light">搜索饿了么商家，商品名称</a>
       </div>
     </cmui-affix>
-    <cmui-slider>
+    <cmui-slider :loop="true" :page="true" :autoplay="true">
       <cmui-slider-item>
         <div class="list list-col5 paddingb100">
           <div v-for="(item,i) in elemSlider1" :key="i" class="list-item text-center">
@@ -18,64 +18,70 @@
           </div>
         </div>
       </cmui-slider-item>
+       <cmui-slider-item >
+        <div class="list list-col5 paddingb100">
+          <div v-for="(item,i) in elemSlider1" :key="i" class="list-item text-center">
+            <img :src="item.src" width="45" class="margint50" />
+            <p class="text-dark fs-12 margint20">{{ item.name }}</p>
+          </div>
+        </div>
+      </cmui-slider-item>
     </cmui-slider>
-    <div class="flex-container  marginh30 padding30" style="background:#f9f9f9">
-      <div class="flex-container-col">
-        <p class="text-black fw-20 fs-16">品质套餐</p>
-        <p class="text-light fs-12 paddingt10">搭配齐全吃的好</p>
-        <router-link to="/login">
-          <p class="text-gloden fs-11 padding10">立即抢购 ></p>
-        </router-link>
+    <div class="marginh30">
+      <div class="flex-container bg-lightgray">
+        <div class="paddingl30">
+          <p class="text-black fw-20 fs-16">品质套餐</p>
+          <p class="text-light fs-12">搭配齐全吃的好</p>
+          <router-link to="/login">
+            <p class="text-gloden fs-11">立即抢购 ></p>
+          </router-link>
+        </div>
+          <img
+            style="width:120px"
+            src="https://cube.elemecdn.com/e/ee/df43e7e53f6e1346c3fda0609f1d3png.png?x-oss-process=image/format,webp/resize,w_282,h_188,m_fixed"
+            alt
+          />
       </div>
-        <img
-          style="width:120px"
-          src="https://cube.elemecdn.com/e/ee/df43e7e53f6e1346c3fda0609f1d3png.png?x-oss-process=image/format,webp/resize,w_282,h_188,m_fixed"
-          alt
-        />
-    </div>
-    <div class="flex-container marginh30 radius4 padding30 margint30" style="background-color:#f3dda0;color:#644f1b">
-      <i class="iconfont icon-gifts"></i>
-      <p class="marginl30">超级会员</p>
-      <p class="fs-10 marginl30 left">每月领取20元红包</p>
-      <div class="flex-container fs-10">
-        <p>立即开通</p>
+      <div class="flex-container padding30 margint30" style="background-color:#f3dda0;color:#644f1b;">
+        <i class="iconfont icon-gifts"></i>
+        <p class="marginl30">超级会员</p>
+        <p class="fs-10 marginl30 left">每月领取20元红包</p>
+        <p class="fs-11">立即开通</p>
         <i class="iconfont icon-more"></i>
       </div>
-    </div>
-    <div class="flex-container round margint30 pos-r">
-      <p class="text-black line">推荐商家</p>
-      <!-- <i class="iconfont icon-faxian"></i> -->
-    </div>
-    <cmui-affix :top="37">
-      <div class="flex-container round marginv30 padding50 text-light fs-14 bg-white">
-        <div v-for="(item, index) in items" :key="index" @click="Tanchuang(item,index)">
-          {{item}}
-          <i class="iconfont icon-more" v-if="index === 0"></i>
+      <div class="flex-container round margint30 pos-r">
+        <p class="text-black line">推荐商家</p>
+      </div>
+      <cmui-affix :top="37">
+        <div class="flex-container round paddingb30 text-light fs-14">
+          <div v-for="(item, index) in items" :key="index" @click="Tanchuang(item,index)">
+            {{item}}
+            <i class="iconfont icon-more" v-if="index === 0"></i>
+          </div>
         </div>
-      </div>
-    </cmui-affix>
-    <product></product>
-
-    <cmui-popup position="top" :visible.sync="visible">
-      <div style="height:500px">
-        <cmui-affix class="bg-blue padding50 pos-r">
-          <i class="iconfont icon-faxian pos-a text-gray marginl280"></i>
-          <a href class="bg-white paddingv40 paddingh300 marginl80 text-light">搜索饿了么商家，商品名称</a>
-        </cmui-affix>
-        <cmui-tabbar @item-click="checkFirst">
-          <cmui-tabbar-item :title="item" v-for="(item, index) in items2" :key="index"></cmui-tabbar-item>
-        </cmui-tabbar>
-        <ul class="flex-container-col left marginl100">
-          <li
-            :class="{'text-blue':index === colorBlue}"
-            class="pos-r"
-            v-for="(item, index) in items3"
-            :key="index"
-            @click="check(index)"
-          >{{item}}</li>
-        </ul>
-      </div>
-    </cmui-popup>
+      </cmui-affix>
+      <product></product>
+      <cmui-popup position="top" :visible.sync="visible">
+        <div style="height:500px">
+          <cmui-affix class="bg-blue padding50 pos-r">
+            <i class="iconfont icon-faxian pos-a text-gray marginl280"></i>
+            <a href class="bg-white paddingv40 paddingh300 marginl80 text-light">搜索饿了么商家，商品名称</a>
+          </cmui-affix>
+          <cmui-tabbar @item-click="checkFirst">
+            <cmui-tabbar-item :title="item" v-for="(item, index) in items2" :key="index"></cmui-tabbar-item>
+          </cmui-tabbar>
+          <ul class="flex-container-col left marginl100">
+            <li
+              :class="{'text-blue':index === colorBlue}"
+              class="pos-r"
+              v-for="(item, index) in items3"
+              :key="index"
+              @click="check(index)"
+            >{{item}}</li>
+          </ul>
+        </div>
+      </cmui-popup>
+    </div>
   </div>
 </template>
 
